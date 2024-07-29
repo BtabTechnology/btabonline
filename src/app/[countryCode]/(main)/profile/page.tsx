864ad2ec7cbbe307308ebc10d4cffd978254ab05
Steve Deals <6135415+SteveDeals@users.noreply.github.com>
2024-07-30 03:51:30 +1000

    navbar template converted

--- src/app/[countryCode]/(main)/profile/page.tsx
+++ src/app/[countryCode]/(main)/profile/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
+import Navbar from "../navbar"
 
 const profile = () => {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl">profile</h1>
       <Link href="/">Go Home</Link>
     </div>
