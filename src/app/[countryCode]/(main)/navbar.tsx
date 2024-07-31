0c6b27ab75b07f9ea7f9e5ddb2f3f4e4e7c06fb9
Steve Deals <6135415+SteveDeals@users.noreply.github.com>
2024-08-01 05:58:15 +1000

    nav

--- src/app/[countryCode]/(main)/navbar.tsx
+++ src/app/[countryCode]/(main)/navbar.tsx
@@ -20,6 +20,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+            {/* Mobile menu button */}
             <button
               type="button"
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -46,19 +47,22 @@ const Navbar = () => {
             </button>
           </div>
 
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <Link href="/" className="flex flex-shrink-0 items-center">
-              <Image
-                className="h-10 w-auto"
-                src="/images/logo-white.png"
-                alt="PropertyPulse"
-                width={40}
-                height={40}
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                PropertyPulse
-              </span>
-            </Link>
+          <div className="flex flex-1 items-center justify-between">
+            <div className="flex items-center">
+              <Link href="/" className="flex flex-shrink-0 items-center">
+                <Image
+                  className="h-10 w-auto"
+                  src="/images/logo-white.png"
+                  alt="PropertyPulse"
+                  width={40}
+                  height={40}
+                />
+                <span className="text-white text-2xl font-bold ml-2">
+                  BtabVendor
+                </span>
+              </Link>
+            </div>
+
             <div className="hidden md:ml-6 md:block">
               <div className="flex space-x-2">
                 <Link
@@ -68,113 +72,109 @@ const Navbar = () => {
                   Home
                 </Link>
                 <Link
-                  href="/properties"
+                  href="/products"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
-                  Properties
+                  Products
                 </Link>
                 <Link
-                  href="/add-property"
+                  href="/add-products"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
-                  Add Property
+                  Add Products
                 </Link>
               </div>
             </div>
-          </div>
 
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                <i className="fa-brands fa-google text-white mr-2"></i>
-                <span>Login or Register</span>
-              </button>
-            </div>
-          </div>
-
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-            <Link href="/messages" className="relative group">
-              <button
-                type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button>
-              <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                2
-              </span>
-            </Link>
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded={isUserMenuOpen}
-                  aria-haspopup="true"
-                  onClick={toggleUserMenu}
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <Image
-                    className="h-8 w-8 rounded-full"
-                    src="/images/profile.png"
-                    alt=""
-                    width={32}
-                    height={32}
-                  />
+            <div className="hidden md:block md:ml-6">
+              <div className="flex items-center">
+                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  <i className="fa-brands fa-google text-white mr-2"></i>
+                  <span>Login or Register</span>
                 </button>
               </div>
+            </div>
 
-              {isUserMenuOpen && (
-                <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  // tabIndex="-1"
+            <div className="flex items-center">
+              <Link href="/messages" className="relative group mr-3">
+                <button
+                  type="button"
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <Link
-                    href="/profile"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    // tabIndex="-1"
+                  <span className="absolute -inset-1.5"></span>
+                  <span className="sr-only">View notifications</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
                   >
-                    Your Profile
-                  </Link>
-                  <Link
-                    href="/saved-properties"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    // tabIndex="-1"
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
+                </button>
+                <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+                  2
+                </span>
+              </Link>
+              <div className="relative ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded={isUserMenuOpen}
+                    aria-haspopup="true"
+                    onClick={toggleUserMenu}
                   >
-                    Saved Properties
-                  </Link>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    // tabIndex="-1"
-                  >
-                    Sign Out
-                  </a>
+                    <span className="absolute -inset-1.5"></span>
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                      className="h-8 w-8 rounded-full"
+                      src="/images/profile.png"
+                      alt=""
+                      width={32}
+                      height={32}
+                    />
+                  </button>
                 </div>
-              )}
+
+                {isUserMenuOpen && (
+                  <div
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button"
+                  >
+                    <Link
+                      href="/profile"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                    >
+                      Your Profile
+                    </Link>
+                    <Link
+                      href="/saved-properties"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                    >
+                      Saved Properties
+                    </Link>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                    >
+                      Sign Out
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
